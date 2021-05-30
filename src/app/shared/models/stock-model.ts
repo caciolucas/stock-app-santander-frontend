ce5380ce315ffc1fdfd00b9ba4d1dcb2e65dafe8
Cacio Lucas <cclucas060901@gmail.com>
2021-05-30 17:16:11 -0300

    Stock list, integration with heroku backend, conditional colors and more

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,12 @@
+class Stock {
+  id: number;
+  name: string;
+  price: number;
+  variation: number;
+  date: string;
+
+  minimum: number;
+  exclusiveMinimum: boolean;
+}
+
+export default Stock;
